13849fffd0da28476dbd369cd2e45d5e6ab7d522
Kim North <mickeymirage@hotmail.com>
2017-10-02 21:51:36 +0100

    Creating and running a unix command in node. Printing out to the console.

--- server.js
+++ server.js
@@ -2,13 +2,23 @@ var express = require('express')
 var app = express()
 var bodyParser = require('body-parser')
 
+
 // app.use(require('./controllers/index.js'))
 
 app.use(express.static('client'))
 
-// app.get('/', function(req, res){
-//   res.sendFile(__dirname + '/client/index.html');
-// });
+// -----------------
+// Execute unix command
+
+var exec = require('child_process').exec;
+
+function puts(error, stdout, stderr) { 
+	console.log(stdout) 
+}
+
+exec("echo \"hello\"", puts);
+
+// ----------------
 
 app.listen(3000, function () {
   console.log('listening on port 3000')
